06db6e1750c0feb6f58713050c08c7ddabc9db57
Judson Junior <judsonjuniorr@gmail.com>
2020-04-28 10:17:21 -0300

    Desafio finalizado

---
+++ src/config/upload.ts
@@ -0,0 +1,19 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { resolve } from 'path';
+
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder, // destino
+    filename(request, file, callback) {
+      // nome do arquivo
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
